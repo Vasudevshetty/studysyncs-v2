408bd6e68278ab83e9263b18a25365da7a98ba1c
Thejas <teju270934@gmail.com>
2024-09-21 23:09:06 +0530

    initial set has been done to build the home page ui with login and logout page

--- client/src/pages/HomeLayout.jsx
+++ client/src/pages/HomeLayout.jsx
@@ -1,5 +1,19 @@
+import Features from "@/components/Home/Features";
+import Footer from "@/components/Home/Footer";
+import Hero from "@/components/Home/Hero";
+import Navbar from "@/components/Home/Navbar";
+import Testimony from "@/components/Home/Testimony";
+
 function HomeLayout() {
-  return <div>home</div>;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <Testimony />
+      <Footer />
+    </>
+  );
 }
 
 export default HomeLayout;
