8084bbe17bcaa9369445ae7ee6099981a6098ede
Thejas <teju270934@gmail.com>
2024-09-29 21:30:20 +0530

    done with the hero section part desinging

--- client/src/components/Home/Hero.jsx
+++ client/src/components/Home/Hero.jsx
@@ -1,41 +1,16 @@
-import {
-  FaEnvelope,
-  FaGithub,
-  FaGraduationCap,
-  FaInstagram,
-  FaLinkedin,
-} from "react-icons/fa";
-import { useEffect, useState } from "react";
+import { FaGithub, FaGraduationCap, FaLinkedin } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import Stats from "./Stats";
 
 function Hero() {
-  const [downloads, setDownloads] = useState(0);
-  const [users, setUsers] = useState(0);
-  const [branches] = useState([
-    "Computer Science Engineering",
-    "Mechanical Engineering",
-    "Civil Engineering",
-    "Electrical Engineering",
-    "Electronics Engineering",
-  ]);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (downloads < 10) setDownloads((prev) => prev + 1);
-      if (users < 20) setUsers((prev) => prev + 1);
-    }, 100);
-
-    return () => clearInterval(interval);
-  }, [downloads, users]);
-
   return (
-    <div className="flex flex-col sm:flex-row gap-8 sm:gap-0 items-center sm:items-start sm:justify-around px-6 sm:px-16 dark:bg-gray-800 dark:text-white ">
-      {/* left side */}
-      <div className="flex flex-col sm:gap-8 gap-4 sm:w-2/3 w-full sm:mt-5">
-        <h1 className="font-bold text-3xl sm:text-4xl leading-tight tracking-widest text-center sm:text-left">
+    <div className="flex flex-col sm:flex-row gap-8 sm:gap-0 items-center sm:items-start sm:justify-around px-6 sm:px-16  dark:bg-gray-800 dark:text-white  rounded-lg ">
+      {/* Left Side - Welcome Text */}
+      <div className="flex-1 flex flex-col justify-center items-center sm:items-start px-4 sm:px-12">
+        <h1 className="font-bold text-3xl sm:text-2xl leading-tight tracking-widest text-center sm:text-left sm:my-2">
           Unlock Academic Resources Seamlessly
         </h1>
-        <p className="text-sm sm:text-base tracking-wide font-medium text-center sm:text-left">
+        <p className="text-sm sm:text-base tracking-wide font-medium text-center sm:text-left max-w-lg mb-6">
           No more hunting for resources. Get curated materials for every
           subject, from beginner to advanced levels. Whether you need detailed
           explanations, practice questions, solved examples, or previous years
@@ -43,58 +18,21 @@ function Hero() {
           your learning experience smoother and more effective. Stay organized
           and focus on what matters most—your studies.
         </p>
-        <div className="flex justify-center sm:justify-start">
-          <Link to="/signup">
-            <button className="border bg-yellow-300 dark:bg-gray-600 py-3 px-5 rounded-lg hover:bg-yellow-400 transition-all">
-              Join Us Now
-            </button>
+        <div className="flex justify-center sm:justify-start ">
+          <Link
+            to="/signup"
+            className="border bg-yellow-300 dark:bg-gray-600 py-3 px-4 sm:px-10 font-semibold rounded-lg hover:bg-yellow-400 transition-all sm:mb-0 "
+          >
+            Join Us Now
           </Link>
         </div>
-
-        {/* Stats Section */}
-        <div className="mt-6">
-          <p className="text-center sm:text-left font-semibold mb-4">
-            Stats Section
-          </p>
-
-          <div className="sm:flex sm:justify-around ">
-            {/*  */}
-            <div className="flex flex-row sm:flex-col justify-around  sm:gap-4 mb-4">
-              <div className="border h-32 w-32 sm:h-48 sm:w-48 text-xs text-center sm:text-lg dark:bg-gray-600 bg-yellow-300 flex items-center justify-center rounded-md">
-                <span className="sm:text-3xl font-semibold text-sm">
-                  {" "}
-                  {downloads}+
-                </span>{" "}
-                downloads
-              </div>
-
-              <div className="border h-32 w-32 sm:h-48 sm:w-48 text-xs text-center sm:text-lg dark:bg-gray-600 bg-yellow-300 flex items-center justify-center rounded-md">
-                <span className="sm:text-3xl font-semibold text-sm">
-                  {users}+
-                </span>
-                users
-              </div>
-            </div>
-            {/*  */}
-            <div className="border h-48 w-full sm:h-[55vh] sm:w-[70vh] text-xs text-center sm:text-lg dark:bg-gray-600 bg-yellow-300  rounded-md">
-              <p className="font-bold uppercase text-sm mt-1 sm:text-lg sm:mt-7">
-                Resources available on these branches,
-              </p>
-              <ul className="sm:flex sm:flex-col sm:mt-4 gap-5  text-xs p-3 sm:text-lg">
-                {branches.map((branch, index) => (
-                  <li className="p-1" key={index}>
-                    {branch}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        </div>
       </div>
-      {/* developed */}
-      <div className="w-full sm:w-64 h-auto sm:h-screen flex flex-col gap-6 p-4 sm:p-0 sm:border-l sm:px-2">
-        <p className="font-bold text-center sm:text-left">Developed by,</p>
 
+      <Stats />
+
+      {/* Developed */}
+      <div className="w-full sm:w-64 h-auto flex flex-col gap-6 p-4 sm:p-0 sm:border-l sm:px-2 ">
+        <p className="font-bold text-center sm:text-left">Developed by,</p>
         <div className="flex sm:flex-col gap-8 sm:gap-4 justify-center sm:justify-start items-center">
           <div className="w-64 sm:mt-5 sm:ml-20 flex flex-col items-center sm:items-start">
             <img
@@ -103,7 +41,7 @@ function Hero() {
               className="rounded-full h-24 w-24"
             />
             <h4 className="text-center sm:text-left">Thejas</h4>
-            <p className="text-sm flex flex-col gap-2 font-semibold tracking-wider">
+            <p className=" flex flex-col gap-2 font-semibold tracking-wider text-xs sm:text-base ">
               <span className="flex gap-2 items-center">
                 <FaGraduationCap size={24} /> Sjce, Mysore
               </span>
@@ -113,23 +51,17 @@ function Hero() {
               <span className="flex gap-2 items-center">
                 <FaGithub size={24} /> <a href="#">Github</a>
               </span>
-              <span className="flex gap-2 items-center">
-                <FaInstagram size={24} /> <a href="#">instagram</a>
-              </span>
-              <span className="flex gap-2 items-center">
-                <FaEnvelope size={22} /> <a href="#">Email</a>
-              </span>
             </p>
           </div>
 
-          <div className="w-64 sm:mt-10 sm:ml-20 flex flex-col items-center sm:items-start">
+          <div className="w-64 sm:my-10 sm:ml-20 flex flex-col items-center sm:items-start">
             <img
               src="https://avatars.githubusercontent.com/u/121664018?v=4"
               alt="Vasudev"
               className="rounded-full h-24 w-24"
             />
             <h4 className="text-center sm:text-left">Vasudev</h4>
-            <p className="text-sm flex flex-col gap-2 font-semibold tracking-wider">
+            <p className="text-xs sm:text-base flex flex-col gap-2 font-semibold tracking-wider">
               <span className="flex gap-2 items-center">
                 <FaGraduationCap size={24} /> Sjce, Mysore
               </span>
@@ -138,12 +70,6 @@ function Hero() {
               </span>
               <span className="flex gap-2 items-center">
                 <FaGithub size={24} /> <a href="#">Github</a>
-              </span>
-              <span className="flex gap-2 items-center">
-                <FaInstagram size={24} /> <a href="#">instagram</a>
-              </span>
-              <span className="flex gap-2 items-center">
-                <FaEnvelope size={22} /> <a href="#">Email</a>
               </span>
             </p>
           </div>
---
+++ client/src/components/Home/Stats.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+
+function Stats() {
+  const [downloads, setDownloads] = useState(0);
+  const [users, setUsers] = useState(0);
+  const [visibleBranches, setVisibleBranches] = useState([]);
+  const [branches] = useState([
+    "Computer Science Engineering",
+    "Mechanical Engineering",
+    "Civil Engineering",
+    "Electrical Engineering",
+    "Electronics Engineering",
+  ]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (downloads < 20) setDownloads((prev) => prev + 1);
+      if (users < 30) setUsers((prev) => prev + 1);
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, [downloads, users]);
+
+  useEffect(() => {
+    let index = 0;
+    const branchInterval = setInterval(() => {
+      setVisibleBranches((prev) => {
+        const newBranches = [...prev];
+        newBranches[index] = branches[index];
+        index = (index + 1) % branches.length;
+        return newBranches;
+      });
+    }, 500);
+
+    return () => clearInterval(branchInterval);
+  }, [branches]);
+
+  return (
+    <div className="">
+      <div className="mt-6">
+        <div className="sm:flex sm:justify-around gap-10 p-5">
+          {/* Stats Section */}
+          <div className="flex flex-row sm:flex-col justify-around gap-4 mb-4">
+            <div className="border h-32 w-32 sm:h-48 sm:w-48 text-xs text-center sm:text-lg dark:bg-gray-600 bg-yellow-300 flex items-center justify-center rounded-md">
+              <span className="sm:text-3xl font-semibold text-sm">
+                {downloads}+
+              </span>
+              downloads
+            </div>
+
+            <div className="border h-32 w-32 sm:h-48 sm:w-48 text-xs text-center sm:text-lg dark:bg-gray-600 bg-yellow-300 flex items-center justify-center rounded-md">
+              <span className="sm:text-3xl font-semibold text-sm">
+                {users}+
+              </span>
+              users
+            </div>
+          </div>
+          {/* Resources Section */}
+          <div className="border h-48 w-full sm:h-[55vh] sm:w-[70vh] text-xs text-center sm:text-lg dark:bg-gray-600 bg-yellow-300 rounded-md">
+            <p className="font-bold uppercase text-sm mt-1 sm:text-lg sm:mt-7">
+              Resources available in these branches:
+            </p>
+            <ul className="sm:flex sm:flex-col sm:mt-4 gap-5 text-xs p-3 sm:text-lg">
+              {branches.map((branch, index) => (
+                <li
+                  key={index}
+                  className={`p-1 ${
+                    visibleBranches[index] ? "block" : "hidden"
+                  }`}
+                >
+                  {branch}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Stats;
